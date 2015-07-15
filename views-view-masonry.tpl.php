e68dea13a282544bdab6059bd210d1ff0fafb80b
Dom <dom.clause@gmail.com>
2015-07-15 16:56:53 +0200

    Issue #2513922 by Dom., leon.nk: Nested Views also get Masonry applied to them

--- views-view-masonry.tpl.php
+++ views-view-masonry.tpl.php
@@ -5,9 +5,10 @@
  */
 ?>
 
-<?php foreach ($rows as $id => $row): ?>
-  <div class="masonry-item<?php if ($classes_array[$id]) print ' ' . $classes_array[$id]; ?>">
-    <?php print $row; ?>
-  </div>
-<?php endforeach; ?>
-
+<div id="<?php print $container_id; ?>">
+  <?php foreach ($rows as $id => $row): ?>
+    <div class="masonry-item<?php if ($classes_array[$id]) print ' ' . $classes_array[$id]; ?>">
+      <?php print $row; ?>
+    </div>
+  <?php endforeach; ?>
+</div>
