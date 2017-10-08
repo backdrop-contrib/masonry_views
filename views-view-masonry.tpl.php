0f41dd7f7e390d707298d29b92bb9e75d6eacdee
Laryn Kragt Bakker - CEDC.org <laryn@cedc.org>
2017-10-08 10:23:23 -0500

    Issue #1: Make sure $rows exists.

--- views-view-masonry.tpl.php
+++ views-view-masonry.tpl.php
@@ -2,6 +2,14 @@
 /**
  * @file
  * Default view template to display content in a Masonry layout.
+ *
+ * - $title: The title of this group of rows.  May be empty.
+ * - $rows: An array of row items. Each row is an array of content.
+ *   $rows are indexed by row number.
+ * - $row_classes: An array of classes to apply to each row, indexed by row
+ *   number. This matches the index in $rows.
+ *
+ * @ingroup views_templates
  */
 ?>
 
@@ -11,11 +19,13 @@
     <?php endif; ?>
   <?php print $prefix ?>
 <?php endif; ?>
-<?php foreach ($rows as $row_count => $row): ?>
-  <div class="masonry-item<?php if ($row_classes[$row_count]) print ' ' . implode(' ', $row_classes[$row_count]); ?>">
-    <?php print $row; ?>
-  </div>
-<?php endforeach; ?>
+<?php if (isset($rows)): ?>
+  <?php foreach ($rows as $row_count => $row): ?>
+    <div class="masonry-item<?php if ($row_classes[$row_count]) print ' ' . implode(' ', $row_classes[$row_count]); ?>">
+      <?php print $row; ?>
+    </div>
+  <?php endforeach; ?>
+<?php endif;?>
 <?php if(isset($grouping) && $grouping): ?>
   <?php print $suffix ?>
 <?php endif;?>
